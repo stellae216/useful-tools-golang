2634f22e5892059a7773c2747e2c5c6e2d4cc4a9
stellae216 <lhj_96@163.com>
2022-07-07 00:11:41 +0800

    add edit_filename.go

---
+++ application/edit_filename.go
@@ -0,0 +1,6 @@
+package application
+
+// EditFileNameByCreateTime 根据文件创建时间批量修改文件名称
+func EditFileNameByCreateTime(dir string) error {
+	return nil
+}
---
+++ unit_test/edit_filename_test.go
@@ -0,0 +1,7 @@
+package unit_test
+
+import "testing"
+
+func TestEditFileNameByCreateTime(t *testing.T) {
+
+}
